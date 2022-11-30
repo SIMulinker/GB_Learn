f09d4136cad2658e3847a62d6c28681322021882
ALeX <triceraptors@mail.ru>
2022-11-30 21:33:53 +0300

    Add first file

---
+++ Lesson_1/Code1.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write(" Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write(" Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if(num1 == num2)
+{
+    Console.WriteLine(" Вообще-то они равны ");
+} else if (num1 > num2)
+{
+    Console.WriteLine(" Первое число больше: " + num1 );
+} else
+{
+    Console.WriteLine(" Второе число больше: " + num2);
+}
