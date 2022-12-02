9b6ee7ff5103f442080bf0b8594bf2460d5d345b
ALeX <triceraptors@mail.ru>
2022-12-02 19:36:17 +0300

    first lesson correct

--- Lesson_1/Code4.cs
+++ Lesson_1/Code4.cs
@@ -9,7 +9,10 @@ for (int i = 1; i <= num1; i++)
     if (i % 2 == 0)
     {
         if (first == true)
-        { Console.Write(" , " + i); }
-        else { first = true; }
+        {Console.Write(" , " + i); }
+        else { 
+            first = true;
+            Console.Write(i);
+        }
     }
 }
