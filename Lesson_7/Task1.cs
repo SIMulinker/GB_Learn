e344787a7d4483e7f0944f74090a455d060c943b
SIMulinker <113474534+SIMulinker@users.noreply.github.com>
2022-12-21 00:30:22 +0300

    Create Task1.cs

---
+++ Lesson_7/Task1.cs
@@ -0,0 +1,62 @@
+/*
+ Задайте двумерный массив размером m×n, 
+ заполненный случайными вещественными числами.
+ */
+bool MANUAL = true;
+int horz = new Random().Next(5, 16);
+int vert = new Random().Next(5, 16);
+int min = new Random().Next(-20, 1);
+int max = new Random().Next(10, 100);
+
+if (MANUAL)
+{
+    Console.Write(" Enter a horizontal size  ");
+    horz = int.Parse(Console.ReadLine()!);
+    Console.Write(" Enter vertical size  ");
+    vert = int.Parse(Console.ReadLine()!);
+    Console.Write(" Enter minimal value  ");
+    min = int.Parse(Console.ReadLine()!);
+    Console.Write(" Enter maximal value  ");
+    max = int.Parse(Console.ReadLine()!);
+}
+else
+{
+    Console.WriteLine($"Hello, World!\r\nSize of ({horz},{vert}) at [{min}..{max}] value is generated");
+}
+
+double[,] matrix = fillArray(vert, horz, min, max);
+Console.Write(matrixToString(matrix));
+
+
+
+
+double[,] fillArray(int size1, int size2, int fmin, int fmax)
+{
+    double[,] field = new double[size1, size2];// int[] linear = new int[number];
+
+    for (int i = 0; i < field.GetLength(0); i++)
+    {
+        for (int j = 0; j < field.GetLength(1); j++)
+        {
+            field[i, j] = new Random().NextDouble() * (fmax - fmin) + fmin;
+        }
+    }
+    return field;
+}
+
+string matrixToString(double[,] arr)
+{
+    string OUT = "";
+    int ilen = arr.GetLength(0);
+    int jlen = arr.GetUpperBound(1);
+    for (int i = 0; i < ilen; i++)
+    {
+        OUT += "( ";
+        for (int j = 0; j < jlen; j++)
+        {
+            OUT += String.Format("{0,6:N2} ", arr[i, j]);
+        }
+        OUT += String.Format("{0,6:N2} )\r\n", arr[i, jlen]);
+    }
+    return OUT;
+}
